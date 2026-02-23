e4a0bdf4022ea3ecf7ec2e5f7e0101cac2bbf360
Brad Carman <bradleygcarman@outlook.com>
2026-02-23 14:52:03 -0500

    v0.2.5 - fix pkgversion problem

--- src/ModelingToolkitInputs.jl
+++ src/ModelingToolkitInputs.jl
@@ -177,7 +177,7 @@ function build_input_functions(sys, inputs)
                                        for x in ModelingToolkit.unwrap.(inputs)]
     setters = []
     events = ModelingToolkit.SymbolicDiscreteCallback[]
-    defaults = if pkgversion(ModelingToolkit) > v"11"
+    defaults = if isdefined(ModelingToolkit, :initial_conditions) # only defined on MTK v11, not v10 and below
         ModelingToolkit.initial_conditions(sys)
     else
         ModelingToolkit.defaults(sys)
@@ -225,7 +225,7 @@ function CommonSolve.solve(input_prob::InputProblem, args...; inputs::Vector{Inp
 
         # DiscreteCallback doesn't hit on t==0, workaround...
         if input.time[1] == 0
-            if pkgversion(ModelingToolkit) > v"11"
+            if isdefined(ModelingToolkit, :initial_conditions) # only defined on MTK v11, not v10 and below
                 prob.ps[Initial(input.var)] = input.data[1]
             else
                 prob.ps[input.var] = input.data[1]
